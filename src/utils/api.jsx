fec57feb9034dda6bb93d984ad551e92390c6fae
wpaulow <paulow.wesley33@gmail.com>
2024-11-05 19:20:09 -0300

    vamo lá

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://moria-desbravadores.brazilsouth.cloudapp.azure.com/",
+    baseURL: "https://moria-desbravadores.brazilsouth.cloudapp.azure.com",
     headers: {
         "Content-Type": "application/json",
     },
