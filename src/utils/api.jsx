8f109c84940f46ee980966fd7d694a502922f885
wpaulow <paulow.wesley33@gmail.com>
2024-11-05 02:26:56 -0300

    quase certeza que vai 2

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 //me ajuda aí
 const api = axios.create({
-    baseURL: "http://4.228.58.203",
+    baseURL: "http://4.228.58.203:8080",
     headers: {
         "Content-Type": "application/json",
     },
